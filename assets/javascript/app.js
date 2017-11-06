892688c1355b4de245ac804e0ef448520d0b1229
Your Name <enzocnop@gmail.com>
2017-11-05 20:01:03 -0800

    Functional App

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,11 +7,10 @@ $(document).ready(function (){
     gif = [];
     gifs = [];
     // displayGif function re-renders the HTML to display the appropriate content
-    $("button").on("click", function() {
-
-    function displayGif() {
+    var mainLoop = function ajaxLoop() {
 
         var gif = $(this).attr("data-name");
+
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&limit=10";
         // Creating an AJAX call for the specific gif button being clicked
         $.ajax({
@@ -44,66 +43,48 @@ $(document).ready(function (){
 
                     gifDiv.append(genGif);
 
-
-
-                    //add data-still, add data-animate, add data-state, add class...
-
-                    // Putting the object onto the page.
-                    $("#gif-view-inside").append(gifDiv);
-                } // closing ratings req.
-            } // closing for loop.
-        }); //closing response function
-    } //closing display Gif
-    displayGif();
-});
-
-      // Function for displaying gif data
-      function renderButtons() {
-
-        // Deleting the gifs prior to adding new gifs
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#gen-buttons-view").empty();
-
-        // Looping through the array of gifs
-        for (var i = 0; i < gifs.length; i++) {
-
-          // Then dynamicaly generating buttons for each gif in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of gif to our button
-          a.addClass("gif");
-          // Adding a data-attribute
-          a.attr("data-name", gifs[i]);
-          // Providing the initial button text
-          a.text(gifs[i]);
-          // Adding the button to the gen-buttons-view div
-          $("#gen-buttons-view").append(a);
-        }
+                    $("#gif-view-inside").prepend(gifDiv);
+                }; // closing ratings req.
+            }; // closing for loop.
+        }) //closing response function
     };
 
+    // $('button').on('click', ajaxLoop());
+
+
+    // Function for displaying gif data
+  function renderButtons() {
+
+    for (var i = 0; i < gifs.length; i++) {
+
+      var a = $("<button>");
+      // Adding a data-attribute
+      a.attr("data-name", gifs[i]);
+      // Providing the initial button text
+      a.text(gifs[i]);
+      // Adding the button to the gen-buttons-view div
+      $("#gen-buttons-view").append(a);
+    }
+};
       // This function handles events where a user gen gif button is clicked
       $("#add-gif").on("click", function(event) {
         event.preventDefault();
         // This line grabs the input from the textbox
         var gif = $("#gif-input").val().trim();
         console.log(gif);
-
         // Adding gif from the textbox to our array
         gifs.push(gif);
-
         // Calling renderButtons which handles the processing of our gif array
         renderButtons();
       });
 
-      // ADD data-state...
       // PAUSING AND UNPAUSING GIFs...
-      $(".gif").on("click", function(event) {
+    var displayGif = function animate(event) {
         event.preventDefault();
+        console.log("State change active!");
       // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
       var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
+
       if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
@@ -111,16 +92,17 @@ $(document).ready(function (){
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
       }
-    });
-        // BUG CLICK LISTENER IS SAME ON ALL FUNCTIONS.
+    };
+
+
         // WE need one for the gifs to pause.unpause
         // one to generate gifs
         // one for the button already on the page
         // gif buttons on page
 
 
-    //   // Adding a click event listener to all elements with a class of "gif"
-    //   $(document).on("click", ".gif", displayGif);
+    $(document).on("click", "img", displayGif);
+    $(document).on("click", "button", mainLoop);
     //   // Calling the renderButtons function to display the intial buttons
     //   renderButtons();
 }); // End Document Ready..
